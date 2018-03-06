3d660cb9fc3a6ab850547f3798c22dfa993ad8f6
Craig Waton <craig@rokkitpress.com>
2018-03-06 20:04:16 +0000

    Remove panel heading wrapper
    Remove the panel heading wrapper on the h4 element and adjust styles to suit.

--- lib/widget-markup.php
+++ lib/widget-markup.php
@@ -5,17 +5,17 @@ add_filter( 'genesis_register_sidebar_defaults', 'bsg_register_sidebar_defaults'
 function bsg_register_sidebar_defaults($defaults) {
     $defaults = array(
         'before_widget' => genesis_markup( array(
-          'open'    => '<section id="%%1$s" class="panel panel-default widget %%2$s">',
+          'open'    => '<section id="%%1$s" class="panel panel-default widget %%2$s"><div class="panel-body">',
           'context' => 'widget-wrap',
           'echo'    => false,
         ) ),
         'after_widget'  => genesis_markup( array(
-          'close'   => '</section>' . "\n",
+          'close'   => '</div></section>' . "\n",
           'context' => 'widget-wrap',
           'echo'    => false
         ) ),
-        'before_title'  => '<header class="panel-heading"><h4 class="panel-title widget-title widgettitle">',
-        'after_title'   => '</h4></header><div class="panel-body">' . "\n",
+        'before_title'  => '<h4 class="widget-title widgettitle">',
+        'after_title'   => '</h4>' . "\n",
     );
     return $defaults;
 }
